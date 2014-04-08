25bbc4f8e3f5f16229f432ff684143df19ebccff
unknown <bob.craker@hotmail.com>
2014-04-09 00:18:34 +1200

    Sales structured correctly

--- Sales/Sales/app/controllers/pagesController.js
+++ Sales/Sales/app/controllers/pagesController.js
@@ -1,12 +1,17 @@
 var locomotive = require('locomotive')
-  , Controller = locomotive.Controller;
+	, Controller = locomotive.Controller;
 
 var pagesController = new Controller();
 
 pagesController.main = function() {
-  this.title = 'Locomotive';
-  this.derp = 'le derp';
-  this.render();
+	this.test = 'le test';
+	this.products = [
+		{ name: 'prawn', price: '$10.00' },
+		{ name: 'pig', price: '$5.00' },
+		{ name: 'chicken', price: '$3.00' },
+		{ name: 'cow', price: '$2.00' }
+	];
+	this.render();
 }
 
 module.exports = pagesController;
