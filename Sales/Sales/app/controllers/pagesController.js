65a70213ebe3ef2c404ea48b147eba890d413b61
unknown <bob.craker@hotmail.com>
2014-04-14 21:47:05 +1200

    data nicely going to knockout
    yep

--- Sales/Sales/app/controllers/pagesController.js
+++ Sales/Sales/app/controllers/pagesController.js
@@ -5,12 +5,14 @@ var pagesController = new Controller();
 
 pagesController.main = function() {
 	this.test = 'le test';
+	
 	this.products = [
 		{ name: 'prawn', price: '$10.00' },
 		{ name: 'pig', price: '$5.00' },
 		{ name: 'chicken', price: '$3.00' },
 		{ name: 'cow', price: '$2.00' }
-	];
+	]
+	
 	this.render();
 }
 
