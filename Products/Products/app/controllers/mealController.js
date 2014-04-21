5fdff1c8dfbe49caa2eea0186c5d3f23803702d2
unknown <bob.craker@hotmail.com>
2014-04-21 17:27:19 +1200

    Prcts meal controller and view initialized

--- Products/Products/app/controllers/mealController.js
+++ Products/Products/app/controllers/mealController.js
@@ -5,8 +5,6 @@ var mealController = new Controller();
 
 mealController.main = function() {
   this.meals = [{name: 'Ratatouille'}];
-  this.drinks = [{name: 'Water'}];
-  this.platters = [{name: 'Sushi Platter'}];
   this.render();
 }
 
