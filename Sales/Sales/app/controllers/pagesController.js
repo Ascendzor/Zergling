cf663972a58ffbcb18980965582deca167469078
unknown <bob.craker@hotmail.com>
2014-04-27 02:05:02 +1200

    Sales now getting meal from the products service

--- Sales/Sales/app/controllers/pagesController.js
+++ Sales/Sales/app/controllers/pagesController.js
@@ -1,19 +1,27 @@
 var locomotive = require('locomotive')
-	, Controller = locomotive.Controller;
+	, Controller = locomotive.Controller
+	, http = require('http');
 
 var pagesController = new Controller();
 
 pagesController.main = function() {
-	this.test = 'le test';
-	
-	this.products = [
-		{ name: 'prawn', price: '10.00' },
-		{ name: 'pig', price: '5.00' },
-		{ name: 'chicken', price: '3.00' },
-		{ name: 'cow', price: '2.00' }
-	]
-	
-	this.render();
+	var self = this;
+	var options = {
+		host: '127.0.0.1',
+		path: '/meal/getAllMeals',
+		port: 1338,
+		method: 'GET'
+	}
+	console.log('send the request');
+	var request = http.request(options, function(response) {
+		response.setEncoding('utf8');
+		response.on('data', function(data) {
+			console.log(data);
+			self.products = JSON.parse(data);
+			self.render();
+		});
+	});
+	request.end();
 }
 
 module.exports = pagesController;
